2256535332588afc613a3300cca3478e09300aa4
rupali <36826017+rupalimo@users.noreply.github.com>
2022-03-01 23:24:03 -0500

    cursor fka twigs detail

---
+++ script.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+}
+
+function draw() {
+  background(255);
+  noStroke();
+  fill(mouseX/8, mouseY/3, mouseY/5);
+  circle(mouseX-1, mouseY-3, 5);
+}
