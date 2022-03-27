2ec2ce8712f49d2d6f9559759bd875f45e73fc1c
rupali <36826017+rupalimo@users.noreply.github.com>
2022-03-27 15:16:38 -0400

    update request club link

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ function setup() {
 function draw() {
   background(255);
   noStroke();
-  fill(mouseX/8, mouseY/3, mouseY/5);
+  fill(mouseX/8, mouseY/5, mouseY/5);
   circle(mouseX-1, mouseY-3, 5);
 }
