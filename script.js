6d75c772eeffb2ee7d9e8a441f109e32d945ce31
rupali <36826017+rupalimo@users.noreply.github.com>
2022-04-22 18:01:48 -0400

    edit

--- script.js
+++ script.js
@@ -3,9 +3,23 @@ function setup() {
 
 }
 
+
+
 function draw() {
-  background(255);
+
+    background(255);
   noStroke();
   fill(mouseX/8, mouseY/5, mouseY/5);
   circle(mouseX-1, mouseY-3, 5);
+
+  //   background(255);
+  // fill(mouseX/8, mouseY/3, mouseY/3);
+  // text("rupali morzaria is a visual designer and film programmer currently based in tiohti:áke or montréal", mouseX-250, mouseY);
+
+  // need to figure out how to add borderbox:
+
+  //   stroke(0);
+  // fill(255);
+  // rectMode(CENTER);
+  // rect(mouseX, mouseY, 200, 20, 8);
 }
