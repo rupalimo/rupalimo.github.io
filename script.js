5c3b8abfed460b5dd552f77304089c16a20229b4
rupali <36826017+rupalimo@users.noreply.github.com>
2022-04-29 21:45:34 -0400

    rgb cursor calculation

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function draw() {
 
     background(255);
   noStroke();
-  fill(mouseX/8, mouseY/5, mouseY/5);
+  fill(mouseX/11, mouseY/5, mouseY/5);
   circle(mouseX-1, mouseY-3, 5);
 
   //   background(255);
